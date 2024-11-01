1c46d53b548ab444b2c22466d4c4031319e65b27
abolfazlj10 <abolfazljamshidi101010@gmail.com>
2024-11-02 00:33:32 +0330

    complete first page

--- app/page.js
+++ app/page.js
@@ -3,7 +3,26 @@ export default function Home() {
   return (
     <>
       <Navbar/>
-      <div className="h-screen bg-gradient-to-b from-[#62CFF4] to-[#2C67F2]">screen 1</div>
+      <div className="screenOne">
+        <div className='grid-cols-subgrid col-span-7'></div>
+        <div className='grid-rows-subgrid col-span-1 row-span-3'></div>
+
+        <div className='containerMainContent'>
+          <div className='titleHeading'>
+            <div className='text-highBlue'>مـــــــــــــــــوج</div>
+            <div className='text-lowBlue'>آرمــــــــــــــــان‌</div>
+          </div>
+          <div className='containerTwoIcon'>
+            <img className='' src='./mowj-Icon.png'/>
+            <img className='' src='./mowj-Icon.png'/>
+          </div>
+          <div className='spaceImageMain'>
+            <img className='Image' src='./mowj-BG.png'/>
+          </div>
+        </div>
+
+        <div className='grid-rows-subgrid col-span-1 row-span-3'></div>
+      </div>
 
       <div className="h-screen bg-highBlue">screen 2</div>
       <div className="h-screen bg-lowBlue">screen 3</div>
