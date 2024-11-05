4e66555d86e9e76f3a4d80ef5b45d37558c51e96
abolfazlj10 <abolfazljamshidi101010@gmail.com>
2024-11-05 10:58:13 +0330

    complete one screen

--- app/page.js
+++ app/page.js
@@ -3,25 +3,32 @@ export default function Home() {
   return (
     <>
       <Navbar/>
-      <div className="screenOne">
-        <div className='grid-cols-subgrid col-span-7'></div>
-        <div className='grid-rows-subgrid col-span-1 row-span-3'></div>
+      <div className="oneScreen">
 
-        <div className='containerMainContent'>
-          <div className='titleHeading'>
-            <div className='text-highBlue'>مـــــــــــــــــوج</div>
-            <div className='text-lowBlue'>آرمــــــــــــــــان‌</div>
-          </div>
-          <div className='containerTwoIcon'>
-            <img className='' src='./mowj-Icon.png'/>
-            <img className='' src='./mowj-Icon.png'/>
-          </div>
-          <div className='spaceImageMain'>
-            <img className='Image' src='./mowj-BG.png'/>
-          </div>
+        <div className="gridSystemOneScreen">
+          <div className='grid-cols-subgrid col-span-7'></div>
+          <div className='grid-rows-subgrid col-span-1 row-span-3'></div>
+
+          <div className='containerMainContent'>
+            <div className='titleHeading'>
+              <div className='text-highBlue'>مـــــــــــــــــوج</div>
+              <div className='text-lowBlue'>آرمــــــــــــــــان‌</div>
+            </div>
+            <div className='containerTwoIcon'>
+              <img className='' src='./mowj-Icon.png'/>
+              <img className='' src='./mowj-Icon.png'/>
+            </div>
+            <div className='spaceImageMain'>
+              <img className='Image' src='./mowj-BG.png'/>
+            </div>            
         </div>
 
         <div className='grid-rows-subgrid col-span-1 row-span-3'></div>
+        </div>
+
+        <div className="arrowDown">
+          <img src='./ChevronsDown.svg' />
+        </div>
       </div>
 
       <div className="h-screen bg-highBlue">screen 2</div>
