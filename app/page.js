3b7bb824ccfd67b48744bc645943f38f60927d0f
abolfazlj10 <abolfazljamshidi101010@gmail.com>
2024-11-05 22:43:36 +0330

    add links to tag social media.

--- app/page.js
+++ app/page.js
@@ -59,14 +59,12 @@ export default function Home() {
         </div>
         <div className='socialMediaLinks'>
           <div className='containerLinks'>
-            <div className='text-[#D44638] mt-14'>جیمیل</div>
-            <div className='text-[#0088CC] mt-5'>تلگرام</div>
-            <div className='instagramLink -mt-10'>اینستاگرام</div>
+            <div className='text-[#D44638] mt-14'><a href='mailto:mowjarman@gmail.com'>جیمیل</a></div>
+            <div className='instagramLink -mt-10'><a href='https://www.instagram.com/mowjarman'>اینستاگرام</a></div>
           </div>
           <div className='containerLinks'>
-            <div className='text-[#0A66C2] mr-32'>لینکدین</div>
-            <div className='text-[#25D366] -mt-10'>واتسپ</div>
-            <div className='text-[#060606] mt-14'>ایکس</div>
+            <div className='text-[#25D366] mr-32'><a href='https://wa.me/+9058618858'>واتسپ</a></div>
+            <div className='text-[#0088CC] mt-5'><a href='https://t.me/MowjArman'>تلگرام</a></div>
           </div>
         </div>
         <div className='flex justify-center'><a href='https://mehdi-arman-s-project.vercel.app/'><img src='./desginer.svg'/></a></div>
