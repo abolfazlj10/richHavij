b138e160bbda2dee4b2ab0cc48c9943badc4f6dc
abolfazlj10 <abolfazljamshidi101010@gmail.com>
2024-11-10 09:01:27 +0330

    replaced dependencies.

--- app/components/onceScreen.js
+++ app/components/onceScreen.js
@@ -5,21 +5,20 @@ export default function OnceScreen () {
                 <div className='grid-cols-subgrid col-span-7'></div>
                 <div className='max-[1330px]:hidden grid-rows-subgrid col-span-1 row-span-3'></div>
                 <div className='containerMainContent'>
-                    <div className='titleHeading max-[1329px]:hidden'>
-                        <div className='text-highBrown'>مـــــــــــــــــوج</div>
-                        <div className='text-lowBrown'>آرمــــــــــــــــان‌</div>
+                    <div className='titleHeading'>
+                        <div className="flex flex-col">
+                            <div className="subTitle text-highBrown mr-auto max-[820px]:mr-0 -mb-6">ریچ</div>
+                            <div className="text-lowBrown">امیرحسین</div>
+                        </div>
+                        <div className="flex flex-col">
+                            <div className="flex-1 opacity-0 select-none">.</div>
+                            <div className="text-highBrown mr-16">آرمان‌نیا</div>
+                            <div className="subTitle text-lowBrown -mt-5  mr-auto">هویج</div>
+                        </div>
                     </div>
-                    {/* <div className='titleHeading max-[899px]:hidden min-[1330px]:hidden'>
-                        <div className='text-highBrown'>مـــــــــــــوج</div>
-                        <div className='text-lowBrown'>آرمــــــــــــان‌</div>
-                    </div>
-                    <div className='titleHeading min-[900px]:hidden'>
-                        <div className='text-highBrown'>مــــــــــوج</div>
-                        <div className='text-lowBrown'>آرمـــــــــان‌</div>
-                    </div>*/}
                     <div className='containerTwoIcon'>
-                        <img className="max-[1330px]:w-[150px]" src='./havij.png'/>
-                        <img className="max-[1330px]:w-[150px]" src='./havij.png'/>
+                        <img className="w-[150px] max-[1330px]:w-[150px]" src='./havij.png'/>
+                        <img className="w-[150px] max-[1330px]:w-[150px]" src='./havij.png'/>
                     </div> 
                     <div className='spaceImageMain'>
                         <img className='Image' src='./richhavich-bg.png'/>
@@ -33,12 +32,16 @@ export default function OnceScreen () {
             <div className="containerMobileOnceScreen min-[750px]:hidden">
                 <div className="titleMobile">
                     <img className="topIconMowjMobile" src="./havij.png" />
-                    <div className="text-highBrown">مـــــوج</div>
-                    <div className="text-lowBrown">آرمـــان‌</div>
+                    <div className="text-highBrown">امیرحسین</div>
+                    <div className="text-lowBrown">آرمان‌نیا</div>
                 </div>
                 <div className='spaceImageMainMobile'>
                     <img className='ImageMobile' src='./richhavich-bg.png'/>
-                </div>     
+                </div>  
+                <div className="subtextMobile">
+                    <div className="text-highBrown">ریچ</div>
+                    <div className="text-lowBrown">هویج</div>
+                </div>   
             </div>
         </div>
     )
