124788ef61062f6716de0b5cc9aac66429ea35d3
abolfazlj10 <abolfazljamshidi101010@gmail.com>
2024-11-10 21:30:10 +0330

    change title.

--- app/components/secondScreen.js
+++ app/components/secondScreen.js
@@ -4,7 +4,7 @@ export default function SecondScreen () {
             <div className='max-[750px]:hidden grid-cols-subgrid col-span-2'></div>
             <div className='aboutTitle'>
                 <img className='fingerprint' src='./fingerprintsecond.svg' />
-                <div className='nameAbout'>موج‌آرمان؟!</div>
+                <div className='nameAbout'>امیرحسین آرمان‌نیا؟!</div>
             </div>
             <div className='max-[750px]:hidden grid-cols-subgrid col-span-1'></div>
             <div className='aboutText'>
